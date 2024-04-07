b17fbc27963c34d5961a5b15466e2643d46a2816
DevBotDiscord <quandev2k7@gmail.com>
2024-04-07 08:12:41 +0700

    hh

--- baitap9new.cpp
+++ baitap9new.cpp
@@ -1,9 +1,20 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-#define read(file) freopen(file, "w", stdin);
-#define write(file) freopen(file, "r", stdout);
+#define read(file) freopen(file, "r", stdin);
+#define write(file) freopen(file, "w", stdout);
 
-int main() { 
-    return 0;
+int main() {
+  read("baitap9.inp");
+  write("baitap9.out");
+  string a;
+  cin >> a;
+  int dem = 0;
+  for (int i = 0; i < a.length(); i++) {
+    if (a[i] == 'x') {
+      dem++;
+    }
+  }
+  cout << dem;
+  return 0;
 }
